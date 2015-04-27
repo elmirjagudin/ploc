3fb6d49e3def7adfce3609eb9a7623d184012dd1
Elmir Jagudin <elmir@unity3d.com>
2015-04-27 02:18:44 -0700

    initial commit

---
+++ ploc/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace ploc
+{
+    class Program
+    {
+        static void process_files(string dir_path)
+        {
+            Console.WriteLine("process files in {0}", dir_path);
+
+            foreach(string dir in Directory.EnumerateDirectories(dir_path))
+            {
+                process_files(dir);
+            }
+
+            foreach(string file in Directory.EnumerateFiles(dir_path))
+            {
+                Console.WriteLine("file: {0}", file);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to ploc!");
+            process_files(Directory.GetCurrentDirectory());
+        }
+    }
+}
